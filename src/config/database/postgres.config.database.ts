c0ef1c7dcf6f933e012b414708bd63d63cc9d60a
Chukwunonso E. N <nonso4emma@yahoo.com>
2025-04-13 18:28:54 +0100

    Update postgres.config.database.ts
    Updated postgress settings to use url instead

--- src/config/database/postgres.config.database.ts
+++ src/config/database/postgres.config.database.ts
@@ -2,11 +2,12 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const POSTGRES_DB_CONFIG: () => TypeOrmModuleOptions = () => ({
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT || '5432'),
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  // host: process.env.POSTGRES_HOST,
+  // port: parseInt(process.env.POSTGRES_PORT || '5432'),
+  // username: process.env.POSTGRES_USER,
+  // password: process.env.POSTGRES_PASSWORD,
+  // database: process.env.POSTGRES_DB,
+  url: process.env.POSTGRES_DATABASE_URL,
   synchronize: true,
   logging: false,
   // entities: ['src/entities/**/*.ts'],
